18f79789654f5d7b2e0dae0d35e0e2d58ddd649a
Yussuf Shariff <yussufshariff@outlook.com>
2023-02-21 16:31:55 +0000

    Basic Styling

--- Components/UserDetails.jsx
+++ Components/UserDetails.jsx
@@ -1,30 +1,24 @@
 import React from "react";
-import {
-  View,
-  StyleSheet,
-  Text,
-  FlatList,
-  Image,
-  ImageBackground,
-} from "react-native";
-import UserCard from "./UserCard";
+import { View, StyleSheet, Text, Image } from "react-native";
 
 const UserDetails = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.name}> Bucket List </Text>
-      <Image
-        source={{
-          uri: "https://whc.unesco.org/uploads/thumbs/site_0252_0008-750-750-20151104113424.jpg",
-        }}
-        style={{ width: 200, height: 200 }}
-      />
-      <Image
-        source={{
-          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/1200px-Colosseo_2020.jpg",
-        }}
-        style={{ width: 200, height: 200 }}
-      />
+      <Text style={styles.title}> Bucket List </Text>
+      <View style={styles.column}>
+        <Image
+          style={styles.images}
+          source={{
+            uri: "https://whc.unesco.org/uploads/thumbs/site_0252_0008-750-750-20151104113424.jpg",
+          }}
+        />
+        <Image
+          style={styles.images}
+          source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/1200px-Colosseo_2020.jpg",
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -34,10 +28,25 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
   },
-  name: {
+  title: {
+    textAlign: "center",
     fontSize: 25,
     fontWeight: "bold",
     padding: 10,
+  },
+  images: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 200,
+    height: 200,
+    margin: 5,
+  },
+  column: {
+    flexDirection: "column",
+    alignItems: "center",
+    width: "95%",
+    // borderWidth: 1,
+    // borderColor: 'blue',
   },
 });
 
