29621a89caebfe44ff1632f37af9fe5cc3d05811
Alexander Timmins <alexanderstimmins@gmail.com>
2023-02-22 11:13:51 +0000

    make a start on the login page

--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -1,14 +1,50 @@
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button , TextInput} from "react-native";
+import React, { useEffect, useState } from "react";
+
 
 export default HomePage = ({ navigation }) => {
+  const [user, setUser] = useState(null)
+  console.log(user)
+  const [password, setPassword] = useState(null)
+
+  const performSearchUser = async (user) => {
+  // This performs a search of user data
+  }
+
+  const checkPassword = async () => {
+    if (user.password === password){}
+    // This performs a search of user data
+    }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Your List</Text>
+      <Text style={styles.title}>Sign in</Text>
+      <TextInput
+          style={styles.searchBar}
+          placeholder="Username"
+          value={setUser}
+          onChangeText={(username) => setUser(username)}
+          onSubmitEditing={performSearchUser}
+        />
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Password"
+          value={setPassword}
+          onChangeText={(password) => setPassword(password)}
+          onSubmitEditing={checkPassword}
+        />
+
       <Button
-        title="Add an item"
+        title="Login"
         onPress={() => navigation.navigate("AddLocation")}
       />
+      <Button
+        title="Create new User"
+        onPress={() => navigation.navigate("NewUserForm")}
+      />
     </View>
+
+
   );
 };
 
@@ -17,5 +53,17 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 40,
+  },searchBar: {
+    borderRadius: 10,
+    margin: 10,
+    color: "#000",
+    borderColor: "#666",
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    height: 45,
+    paddingHorizontal: 10,
+    fontSize: 18,
   },
 });
+
+
