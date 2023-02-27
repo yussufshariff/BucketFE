c6efd96b1b57ca204e9833fc58745814461dff1e
Yussuf Shariff <yussufshariff@outlook.com>
2023-02-27 15:21:28 +0000

    Can add to your bucket-list

--- Frontend/Components/AddToBucket.jsx
+++ Frontend/Components/AddToBucket.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import { Text, TouchableOpacity, StyleSheet } from "react-native";
-import { useContext } from 'react'
+import { useContext } from "react";
 import UserContext from "../Contexts/userContext";
+import axios from "axios";
 
 const AddToBucket = ({ locationData, onPressAdd }) => {
-  const loggedInUser = useContext(UserContext)
+  const loggedInUser = useContext(UserContext);
+  const user = loggedInUser.username;
+
+  // console.log(locationData.display_name);
+  // console.log(locationData.lon);
+  // console.log(locationData.lang);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const patchBody = {
+      name: locationData.display_name,
+      coordinates: locationData.lon,
+    };
+    axios.patch(
+      `https://red-muddy-woodpecker.cyclic.app/api/${user}/list`,
+      patchBody
+    );
+  };
+
   return (
-    <TouchableOpacity style={styles.add} onPress={onPressAdd}>
-      <Text>Add To List</Text>
+    <TouchableOpacity style={styles.add} onPress={handleSubmit}>
+      <Text>Add To Your Bucket List</Text>
     </TouchableOpacity>
   );
 };
