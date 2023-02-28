5a4f8c106ac4ea3cdedc8554735437977e6b0b14
Yussuf Shariff <yussufshariff@outlook.com>
2023-02-28 12:32:15 +0000

    Return Comments for Google

--- Frontend/Components/LocationDetails.jsx
+++ Frontend/Components/LocationDetails.jsx
@@ -1,15 +1,35 @@
-import { Dimensions, Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { useContext, useEffect } from "react";
+import UserContext from "../Contexts/userContext";
+import axios from "axios";
 
 function LocationDetails({ route }) {
   const { locationData } = route.params;
+  const loggedInUser = useContext(UserContext);
+  const username = loggedInUser.username;
 
-  return (
-    <View>
-      <Text style={styles.title}>
-        {locationData.display_name.replace(/,.*/, "")}
-      </Text>
-    </View>
-  );
+  const getAllComments = () => {
+    return axios
+      .get(
+        `https://red-muddy-woodpecker.cyclic.app/api/63fcc69c29bb15efca4ea6ba/comments`
+      )
+      .then((response) => {
+        console.log(response.data.comments);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  getAllComments();
+
+  // return (
+  //   <View>
+  //     <Text style={styles.title}>
+  //       {locationData.display_name.replace(/,.*/, "")}
+  //     </Text>
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
