b703f9d0e103a1bf243c388c1ae4be5532f0a505
Abubakr Vali <abubakrvali@Abubakrs-MacBook-Air.local>
2023-03-01 11:19:10 +0000

    Its all a mess

--- Frontend/Components/LocationDetails.jsx
+++ Frontend/Components/LocationDetails.jsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, Alert } from "react-native";
+import { Text, View, StyleSheet, Alert, TouchableOpacity } from "react-native";
 import { useContext, useEffect, useState } from "react";
 import UserContext from "../Contexts/userContext";
 import axios from "axios";
@@ -8,80 +8,77 @@ function LocationDetails({ route }) {
   const [locationComments, setlocationComments] = useState({});
   const { locationData } = route.params;
   const loggedInUser = useContext(UserContext);
+  const handleNewComment = () => {
+    Alert.prompt(
+      "Add Comment",
+      "Enter your comment",
+      [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "OK",
+          onPress: (input) => {
+            const postBody = {
+              locationId: location.data.location[0]._id,
+              userId: loggedInUser._id,
+              body: input,
+              images: "kebab",
+            };
+            return axios.post(
+              `https://red-muddy-woodpecker.cyclic.app/api/comments`,
+              postBody
+            );
+          },
+        },
+      ],
+      "plain-text"
+    );
+  };
 
-  useEffect(() => {
+  const getComments = async () => {
     let location = null;
     let comments = null;
-    const getComments = async () => {
-      try {
-        location = await axios.get(
-          `https://red-muddy-woodpecker.cyclic.app/api/${locationData.display_name}`
-        );
-      } catch {}
-
-      try {
-        comments = await axios.get(
-          `https://red-muddy-woodpecker.cyclic.app/api/${location.data.location[0]._id}/comments`
-        );
-      } catch {}
-    };
-    const handleNewComment = () => {
-      Alert.prompt(
-        "Add Comment",
-        "Enter your comment",
-        [
-          {
-            text: "Cancel",
-            style: "cancel",
-          },
-          {
-            text: "OK",
-            onPress: (input) => {
-              const postBody = {
-                locationId: location.data.location[0]._id,
-                userId: loggedInUser._id,
-                body: "kebab",
-                images: "kebab",
-              };
-              return axios.post(
-                `https://red-muddy-woodpecker.cyclic.app/api/comments`,
-                postBody
-              );
-            },
-          },
-        ],
-        "plain-text"
+    try {
+      location = await axios.get(
+        `https://red-muddy-woodpecker.cyclic.app/api/${locationData.display_name}`
       );
-    };
+    } catch {}
 
-    getComments()
-      .then(() => {
-        if (location === null) {
-          alert("Location not in DB ");
-        } else if (comments === null) {
-          Alert.alert(
-            "Comments not found",
-            "Be the first to comment",
-            [
-              {
-                text: "Cancel",
-                onPress: () => console.log("Cancel Pressed"),
-                style: "cancel",
-              },
-              {
-                text: "Add Comment",
-                onPress: () => handleNewComment(),
-              },
-            ],
-            { cancelable: false }
-          );
-        } else {
-          console.log(comments.data);
-          setlocationComments(comments.data.comments[0]);
-        }
-      })
-      .catch(console.error);
-  }, [locationData]);
+    try {
+      comments = await axios.get(
+        `https://red-muddy-woodpecker.cyclic.app/api/${location.data.location[0]._id}/comments`
+      );
+    } catch {}
+  };
+  getComments()
+    .then(() => {
+      if (location === null) {
+        alert("Location not in DB ");
+      } else if (comments === null) {
+        Alert.alert(
+          "Comments not found",
+          "Be the first to comment",
+          [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel Pressed"),
+              style: "cancel",
+            },
+            {
+              text: "Add Comment",
+              onPress: () => handleNewComment(),
+            },
+          ],
+          { cancelable: false }
+        );
+      } else {
+        console.log(comments.data);
+        setlocationComments(comments.data.comments[0]);
+      }
+    })
+    .catch(console.error);
 
   console.log(locationComments, "<<<<<<<");
   return (
@@ -92,7 +89,9 @@ function LocationDetails({ route }) {
 
       <Text style={styles.title}>{locationComments.body} </Text>
       <Text style={styles.title}>{locationComments.owner} </Text>
-      {/* <Text style={styles.title}>{locationComments.images} </Text> */}
+      <TouchableOpacity onPress={() => handleNewComment()}>
+        <Text>ADD COMMENT</Text>
+      </TouchableOpacity>
     </View>
   );
 }
