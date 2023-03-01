d4614f9f07c3446683525222f3bdf91ad3f5ed84
Lee Hamo <Lee@Matthews-MacBook-Air.local>
2023-03-01 13:44:40 +0000

    user profile pic

--- Frontend/Components/UserProfile.jsx
+++ Frontend/Components/UserProfile.jsx
@@ -4,9 +4,16 @@ import UserContext from "../Contexts/userContext";
 
 export default function UserProfile() {
     const loggedInUser = useContext(UserContext);
+    const imageUrl = loggedInUser.profile_picture;
+
     return (
-        <View>
-            <Image source={loggedInUser.profile_picture} style={styles.profilePicture}></Image>
+        <View style={styles.header_style}>
+            <Image
+        style={styles.avatar}
+        source={{
+          uri: imageUrl
+        }}
+      />
         </View>
     )
 }
@@ -17,5 +24,20 @@ const styles = StyleSheet.create({
         height: 35,
         borderRadius: 24,
         marginRight: 16,
-    }
+    },
+    avatar: {
+        width: 130,
+        height: 130,
+        borderRadius: 63,
+        borderWidth: 4,
+        borderColor: "white",
+        margin: 10,
+      },
+      header_style: {
+        textAlign: "center",
+        backgroundColor: "#B2C2D2",
+        padding: 20,
+        fontSize: 20,
+        alignItems: "center"
+      },
 })  
